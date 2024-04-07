ddbebe970e14472267e35e236652249e6dc9e5f4
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2024-04-07 23:13:48 +0530

    useState implimentation

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './component/Navbar';
+import TextForm from './component/TextForm';
+
+
+function App() {
+  return (
+    <>
+     <Navbar title='Text'/>
+     <div className='container my-3'>
+      <TextForm heading="Enter the text to analyze"/>
+     </div>
+    </>
+  );
+}
+
+export default App;
