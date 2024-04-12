1581908efc074fa4218aa8b62a65807ae8ff2418
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2024-04-12 15:57:52 +0530

    React router implimentation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,21 @@
+import React from 'react';
 import './App.css';
-//import About from './component/About';
+import About from './component/About';
 import Navbar from './component/Navbar';
 import TextForm from './component/TextForm';
-import Alert from './component/Alert';
-
-import React,{useState} from 'react';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const[mode,setMode]=useState('light');
-  const[alert,setAlert]=useState(null);
-  const showAlert=(message,type)=>{
-setAlert({
-  msg:message,
-  type:type
-})
-setTimeout(()=>{
-setAlert(null);
-},3000)
-  }
-  const togglemode = ()=>{
-    if(mode === "light"){
-      setMode("dark")
-      document.body.style.backgroundColor = "black"
-      document.body.style.color = "white"
-      showAlert("dark mode enabled","sucess")
-    }else{
-      setMode("light");
-      document.body.style.backgroundColor = "white"
-      document.body.style.color = "black"
-      showAlert("light mode eabled","sucess");
-    }
-  }
   return (
-    <>
-     <Navbar title='Text'mode={mode}togglemode={togglemode}/>
-<Alert alert={alert}/>
-
-     <div className='container my-3'>
-      {<TextForm showAlert={showAlert} heading="Enter the text to analyze"/>}
-      {/*<About></About>*/}
-     </div>
-    </>
+    <Router>
+      
+        <Navbar />
+        <Routes>
+          <Route path="/About" element={<About />} />
+          <Route path="/TextForm" element={<TextForm />} />
+        </Routes>
+      
+    </Router>
   );
 }
 
